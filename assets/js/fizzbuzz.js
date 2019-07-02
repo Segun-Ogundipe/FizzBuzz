c333b6e3f9434c82ced9893f393b4287b6b48e61
Segun Ogundipe <davephenoms@gmail.com>
2019-07-02 16:10:27 +0100

    chore:remove hiding start button

--- assets/js/fizzbuzz.js
+++ assets/js/fizzbuzz.js
@@ -3,7 +3,7 @@ let start = document.getElementById('start');
 
 document.getElementById('start').addEventListener('click', () => {
 
-  for (let i = 1; i <= 5; i++) {
+  for (let i = 1; i <= 100; i++) {
       let result = '';
       result += (i % 3) === 0 ? 'Fizz' : '';
       result += (i % 5) === 0 ? 'Buzz' : '';
